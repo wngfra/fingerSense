963ac4516c1d7628c20be2b365dcb98a53c6b3a9
wngfra <wngfra@gmail.com>
2020-07-03 17:38:24 +0200

    added init functions for FrankaHW

--- ros2_ws/src/franka_control/include/franka_hw.h
+++ ros2_ws/src/franka_control/include/franka_hw.h
@@ -1,27 +1,92 @@
 #pragma once
 
 #include <atomic>
+#include <memory>
+#include <string>
+#include <vector>
 
 #include <rclcpp/rclcpp.hpp>
 
 namespace franka_hw
 {
-    // Singleton class for franka hardware abstraction
+    /**
+    * This class wraps the functionality of libfranka for controlling Panda robots into the ros_control
+    * framework.
+    */
     class FrankaHW
     {
     public:
-        FrankaHW(rclcpp::Node::sharedPtr, string &);
-        // ~FrankaHW();
+        FrankaHW();
+        virtual ~FrankaHW();
 
-        void update();
-        void control();
-        void reset(rclcpp::Node::sharedPtr);
+        /**
+        * Initializes the FrankaHW class to be fully operational. This involves parsing required
+        * configurations from the ROS parameter server, connecting to the robot and setting up interfaces
+        * for the ros2_control framework.
+        *
+        * @param[in] nh A node handle in the root namespace of the control node.
+        * 
+        * @return True if successful, false otherwise.
+        */
+        virtual bool init(std::shared_ptr<rclcpp::Node> nh);
 
-    private:
-        std::atomic_bool has_error = false;
-        std::atomic_bool has_instance = false;
-        ;
-        franka::Robot robot;
-    }
+        /**
+        * Reads the parameterization of the hardware class from the ROS parameter server
+        * (e.g. arm_id, robot_ip joint_names etc.)
+        *
+        * @param[in] nh A node handle in the root namespace of the control node..
+        *
+        * @return True if successful, false otherwise.
+        */
+        virtual bool initParameters(std::shared_ptr<rclcpp::Node> nh);
+
+        /**
+        * Uses the robot_ip_ to connect to the robot via libfranka and loads the libfranka model.
+        */
+        virtual void initRobot();
+
+        virtual void update();
+        virtual void control();
+
+    protected:
+        /**
+        * Parses a set of collision thresholds from the parameter server. The methods returns
+        * the default values if no parameter was found or the size of the array did not match
+        * the defaults dimension.
+        *
+        * @param[in] name The name of the parameter to look for.
+        * @param[in] robot_hw_nh A node handle in the namespace of the robot hardware.
+        * @param[in] defaults A set of default values that also specify the size the parameter must have
+        * to be valid.
+        * @return A set parsed parameters if valid parameters where found, the default values otherwise.
+        */
+        static std::vector<double> getCollisionThresholds(const std::string &name,
+                                                          std::shared_ptr<rclcpp::Node> nh,
+                                                          const std::vector<double> &defaults);
+
+        struct CollisionConfig
+        {
+            std::array<double, 7> lower_torque_thresholds_acceleration;
+            std::array<double, 7> upper_torque_thresholds_acceleration;
+            std::array<double, 7> lower_torque_thresholds_nominal;
+            std::array<double, 7> upper_torque_thresholds_nominal;
+            std::array<double, 6> lower_force_thresholds_acceleration;
+            std::array<double, 6> upper_force_thresholds_acceleration;
+            std::array<double, 6> lower_force_thresholds_nominal;
+            std::array<double, 6> upper_force_thresholds_nominal;
+        };
+
+        CollisionConfig collision_config_;
+        franka::CartesianPose cartP_ros2command_;
+        franka::CartesianVelocities cartV_ros2command_;
+
+        bool initialized_{false};
+        std::atomic_bool controller_active_{false};
+
+        std::unique_ptr<franka::Robot> robot_;
+        std::unique_ptr<franka::Model> model_;
+
+        std::string robot_ip_;
+    };
 
 } // namespace franka_hw
--- ros2_ws/src/franka_control/src/franka_hw.cpp
+++ ros2_ws/src/franka_control/src/franka_hw.cpp
@@ -1,4 +1,6 @@
-#inlclude "franka_hw.cpp"
+#include <exception>
+#include <mutex>
+#include <vector>
 
 #include <franka/control_types.h>
 #include <franka/duration.h>
@@ -6,49 +8,143 @@
 #include <franka/robot.h>
 #include <franka/robot_state.h>
 
+#include <rclcpp/rclcpp.hpp>
+
+#include "franka_hw.h"
+
 namespace franka_hw
 {
-    FrankaHW::FrankaHW(rclcpp::Node::sharedPtr node_ptr, string &robot_ip)
+    FrankaHW::FrankaHW()
+        : cartP_ros2command_({1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0}),
+          cartV_ros2command_({0.0, 0.0, 0.0, 0.0, 0.0, 0.0}) {}
+
+    bool FrankaHW::init(std::shared_ptr<rclcpp::Node> nh)
     {
-        if (!has_instance)
+        if (initialized_)
         {
-            try
-            {
-                robot = franka::Robot(robot_ip);
-                has_error = false;
-                has_instance = true;
-            }
-            catch (franka::Exception const &e)
-            {
-                has_error = true;
-                RCLCPP_WARN(node_ptr->get_logger(), e.what());
-            }
+            RCLCPP_ERROR(nh->get_logger(), "FrankaHW: Cannot be initialized twice.");
+            return false;
         }
-    }
 
-    FrankaHW::update()
-    {
-    }
-
-    FrankaHW::control()
-    {
-    }
-
-    FrankaHW::reset(rclcpp::Node::sharedPtr node_ptr)
-    {
-        if (has_error)
+        if (!initParameters(nh))
         {
-            try
-            {
-                robot.automaticErrorRecovery();
-                has_error = false;
-            }
-            catch (franka::Exception const &e)
-            {
-                RCLCPP_WARN(node_ptr->get_logger(), e.what());
-                RCLCPP_ERROR("Automatic recovery failed.");
-            }
+            RCLCPP_ERROR(nh->get_logger(), "FrankaHW: Failed to parse all required parameters.");
+            return false;
         }
+        try
+        {
+            initRobot();
+        }
+        catch (const std::runtime_error &error)
+        {
+            RCLCPP_ERROR(nh->get_logger(), "FrankaHW: Failed to initialize libfranka robot. %s", error.what());
+            return false;
+        }
+
+        initialized_ = true;
+        return true;
+    }
+
+    bool FrankaHW::initParameters(std::shared_ptr<rclcpp::Node> nh)
+    {
+        bool rate_limiting;
+        if (!nh.get_parameters("rate_limiting", rate_limiting))
+        {
+            RCLCPP_ERROR(nh->get_logger(), "Invalid or no rate_limiting parameter provided");
+            return false;
+        }
+
+        double cutoff_frequency;
+        if (!nh.get_parameters("cutoff_frequency", cutoff_frequency))
+        {
+            RCLCPP_ERROR(nh->get_logger(), "Invalid or no cutoff_frequency parameter provided");
+            return false;
+        }
+
+        std::string internal_controller;
+        if (!nh.get_parameters("internal_controller", internal_controller))
+        {
+            RCLCPP_ERROR(nh->get_logger(), "No internal_controller parameter provided");
+            return false;
+        }
+
+        if (!nh.get_parameters("robot_ip", robot_ip_))
+        {
+            RCLCPP_ERROR(nh->get_logger(), "Invalid or no robot_ip parameter provided");
+            return false;
+        }
+
+        if (!nh.get_parameters("joint_limit_warning_threshold", joint_limit_warning_threshold_))
+        {
+            RCLCPP_INFO(nh->get_logger(),
+                        "No parameter joint_limit_warning_threshold is found, using default "
+                        "value %f",
+                        joint_limit_warning_threshold_);
+        }
+
+        // Get full collision behavior config from the parameter server.
+        std::vector<double> thresholds =
+            getCollisionThresholds("lower_torque_thresholds_acceleration", nh,
+                                   {20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.lower_torque_thresholds_acceleration.begin());
+        thresholds = getCollisionThresholds("upper_torque_thresholds_acceleration", nh,
+                                            {20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.upper_torque_thresholds_acceleration.begin());
+        thresholds = getCollisionThresholds("lower_torque_thresholds_nominal", nh,
+                                            {20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.lower_torque_thresholds_nominal.begin());
+        thresholds = getCollisionThresholds("upper_torque_thresholds_nominal", nh,
+                                            {20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.upper_torque_thresholds_nominal.begin());
+        thresholds.resize(6);
+        thresholds = getCollisionThresholds("lower_force_thresholds_acceleration", nh,
+                                            {20.0, 20.0, 20.0, 25.0, 25.0, 25.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.lower_force_thresholds_acceleration.begin());
+        thresholds = getCollisionThresholds("upper_force_thresholds_acceleration", nh,
+                                            {20.0, 20.0, 20.0, 25.0, 25.0, 25.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.upper_force_thresholds_acceleration.begin());
+        thresholds = getCollisionThresholds("lower_force_thresholds_nominal", nh,
+                                            {20.0, 20.0, 20.0, 25.0, 25.0, 25.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.lower_force_thresholds_nominal.begin());
+        thresholds = getCollisionThresholds("upper_force_thresholds_nominal", nh,
+                                            {20.0, 20.0, 20.0, 25.0, 25.0, 25.0});
+        std::copy(thresholds.begin(), thresholds.end(),
+                  collision_config_.upper_force_thresholds_nominal.begin());
+
+        return true;
+    }
+
+    FrankaHW::update() {}
+
+    FrankaHW::control() {}
+
+    FrankaHW::reset() {}
+
+    std::vector<double> FrankaHW::getCollisionThresholds(const std::string &name,
+                                                         std::shared_ptr<rclcpp::Node> nh,
+                                                         const std::vector<double> &defaults)
+    {
+        std::vector<double> thresholds;
+        if (!nh.get_parameters("collision_config/" + name, thresholds) ||
+            thresholds.size() != defaults.size())
+        {
+            std::string message;
+            for (const double &threshold : defaults)
+            {
+                message += std::to_string(threshold);
+                message += " ";
+            }
+            RCLCPP_INFO(nh->get_logger(), "No parameter %s found, using default values: %s", name.c_str(), message.c_str());
+            return defaults;
+        }
+        return thresholds;
     }
 
 } // namespace franka_hw
