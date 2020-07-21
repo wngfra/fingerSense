43552c4bd6e0950f0e50a23c224c9fbcbafdec1c
wngfra <wngfra@gmail.com>
2020-07-22 00:35:03 +0200

    [TODO]add ControllerInterface as middleware of robot control and cognitive processing

---
+++ franka_control/include/franka_control/ControllerInterface.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace franka_control
+{
+    class ControllerInterface
+    {
+    public:
+        ControllerInterface();
+
+    private:
+    };
+} // namespace franka_control
