6fc631050bada6382b8af0f8836a6f00253b12e5
wngfra <wngfra@gmail.com>
2020-10-09 17:52:36 +0200

    added reciprocating horizontal sliding

--- franka_control/src/mafia.cpp
+++ franka_control/src/mafia.cpp
@@ -66,125 +66,72 @@ int main(int argc, char **argv)
         robot.control(motion_generator);
 
         /*
-         * Control loop
-         * REVIEW: composite action
-         * TODO: action gradient
+         *
+         * TODO sliding motion
+         * 
          */
-        // Set up action bases
-        int n = 31;
-        Eigen::MatrixXd bound(6, 2);
-        bound << -0.001,          0.001,
-                 -0.001,          0.001,
-                 -0.001,          0.001,
-                 -M_PI_4 / 100, M_PI_4 / 100,
-                 -M_PI_4 / 100, M_PI_4 / 100,
-                 -M_PI_4 / 100, M_PI_4 / 100;
-        Eigen::MatrixXd weights(6, n);
-        std::vector<std::function<double(const double &)>> bases;
-        franka_control::Action action(bound);
 
-        for (int i = 0; i < n; ++i)
-        {
-            double omega = (i + 1) / 2;
-            double coeff = (i + 1) % 2;
-
-            weights.col(i) << Eigen::VectorXd::Random(6) * 1e-5;
-            bases.push_back([&](const double &t) -> double {
-                return (1.0 - coeff) * sin(omega * 10 * M_PI * t) + coeff * cos(omega * 10 * M_PI * t);
-            });
-        }
-        
-
-        bool res = action.addBases(bases, weights);
-        auto generated_pos = action(0.0);
-
-        // Compliance parameters
-        double translational_stiffness{150.0};
-        double rotational_stiffness{60.0};
-        Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
-        stiffness.setZero();
-        stiffness.topLeftCorner(3, 3) << translational_stiffness * Eigen::MatrixXd::Identity(3, 3);
-        stiffness.bottomRightCorner(3, 3) << rotational_stiffness * Eigen::MatrixXd::Identity(3, 3);
-        damping.setZero();
-        damping.topLeftCorner(3, 3) << 2.0 * sqrt(translational_stiffness) *
-                                           Eigen::MatrixXd::Identity(3, 3);
-        damping.bottomRightCorner(3, 3) << 2.0 * sqrt(rotational_stiffness) *
-                                               Eigen::MatrixXd::Identity(3, 3);
-
-        franka::RobotState initial_state = robot.readOnce();
-        auto desired_pose = initial_state.O_T_EE;
-
+        double x_max = 0.3;
         double time = 0.0;
-        // define callback for the torque control loop
-        std::function<franka::Torques(const franka::RobotState &, franka::Duration)> cartesian_impedance_control_callback = [&](const franka::RobotState &robot_state, franka::Duration period) -> franka::Torques {
+        double v_max = 0.3;
+        double accel_x = v_max * v_max / 1.5; // max_ddx guarantee
+        double omega = v_max / accel_x;
+        double accel_time = M_PI_2 / omega;
+        double const_v_time = (x_max - 2 * accel_x) / v_max;
+
+        int cycle_count = 0;
+        int cycle_max = 3;
+
+        // define callback for the cartesian control loop
+        std::function<franka::CartesianVelocities(const franka::RobotState &, franka::Duration)> cartesian_velocities_control_callback = [&](const franka::RobotState &robot_state, franka::Duration period) -> franka::CartesianVelocities {
             time += period.toSec();
 
-            // equilibrium point is moving down
-            Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(desired_pose.data()));
-            Eigen::Vector3d position_d(initial_transform.translation());
-            Eigen::Quaterniond orientation_d(initial_transform.linear());
+            double v_x = 0.0;
 
-            // get state variables
-            std::array<double, 7> coriolis_array = model.coriolis(robot_state);
-            std::array<double, 42> jacobian_array =
-                model.zeroJacobian(franka::Frame::kEndEffector, robot_state);
-
-            // convert to Eigen
-            Eigen::Map<const Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
-            Eigen::Map<const Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
-            Eigen::Map<const Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
-            Eigen::Map<const Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
-            Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
-            Eigen::Vector3d position(transform.translation());
-            Eigen::Quaterniond orientation(transform.linear());
-
-            // compute error to desired equilibrium pose
-            // position error
-            Eigen::Matrix<double, 6, 1> error;
-            error.head(3) << position - position_d;
-
-            // orientation error
-            // "difference" quaternion
-            if (orientation_d.coeffs().dot(orientation.coeffs()) < 0.0)
+            if (time <= accel_time)
             {
-                orientation.coeffs() << -orientation.coeffs();
+                v_x = v_max * std::sin(omega * time);
             }
-            // "difference" quaternion
-            Eigen::Quaterniond error_quaternion(orientation.inverse() * orientation_d);
-            error.tail(3) << error_quaternion.x(), error_quaternion.y(), error_quaternion.z();
-            // Transform to base frame
-            error.tail(3) << -transform.linear() * error.tail(3);
-
-            // compute control
-            Eigen::VectorXd tau_task(7), tau_d(7);
-
-            // Spring damper system with damping ratio=1
-            tau_task << jacobian.transpose() * (-stiffness * error - damping * (jacobian * dq));
-            tau_d << tau_task + coriolis;
-
-            std::array<double, 7> tau_d_array{};
-            Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
-
-            if (time >= 30.0)
+            else if (time <= const_v_time + accel_time)
             {
-                RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Finished motion, shutting down controller");
-                franka::Torques torques(tau_d_array);
-                return franka::MotionFinished(torques);
+                v_x = v_max;
+            }
+            else if (time <= const_v_time + 3 * accel_time)
+            {
+                double t = time - const_v_time;
+                v_x = v_max * std::sin(omega * t);
+            }
+            else if (time <= 2 * const_v_time + 3 * accel_time)
+            {
+                v_x = -v_max;
+            }
+            else if (time <= 2 * const_v_time + 4 * accel_time)
+            {
+                double t = time - 2 * const_v_time;
+                v_x = v_max * std::sin(omega * t);
             }
 
-            generated_pos = action(time);
-            desired_pose[12] += generated_pos[0];
-            desired_pose[13] += generated_pos[1];
-            desired_pose[14] += generated_pos[2];
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "dx: %f", generated_pos[0]);
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "dy: %f", generated_pos[1]);
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "dz: %f", generated_pos[2]);
+            franka::CartesianVelocities output = {{v_x, 0.0, 0.0, 0.0, 0.0, 0.0}};
 
-            return tau_d_array;
+            if (time >= 2 * const_v_time + 4 * accel_time)
+            {
+                time -= 2 * const_v_time + 4 * accel_time;
+                cycle_count += 1;
+            }
+
+            if (cycle_count >= cycle_max)
+            {
+                return franka::MotionFinished(output);
+            }
+
+            return output;
         };
 
         // start real-time control loop
-        robot.control(cartesian_impedance_control_callback);
+        if (accel_x < x_max)
+        {
+            robot.control(cartesian_velocities_control_callback);
+        }
 
         RCLCPP_INFO(rclcpp::get_logger("libfranka"), "Touched the platform!");
 
