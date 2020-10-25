bad16ceaa25141e73b7252b06e5c2ecbbd8acf83
wngfra <wngfra@gmail.com>
2020-10-25 18:18:20 +0100

    added visualization package to docker image

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run --gpus all -it --tty --user ubuntu --name $1 -v $(realpath ~)/ros2_ws:/home/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -w /home/ubuntu/ros2_ws wngfra/ros2cuda:libfranka
+docker run --gpus all -it --tty --user ubuntu --name $1 -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix wngfra/ros2cuda:libfranka
