ac45455c60e8f30ddd109cda442cbeaa37fb28bf
wngfra <wngfra@gmail.com>
2020-10-25 23:23:17 +0100

    [TODO] add latent space visualization

--- finger_sense/finger_sense/percepy.py
+++ finger_sense/finger_sense/percepy.py
@@ -4,6 +4,10 @@
 import numpy as np
 from numpy.fft import fft
 
+from mayavi import mlab
+from tensorly.decomposition import tucker
+
+
 def fourier_cov(x):
     y = fft(x, axis=0) / x.shape[0]
     ys = y[:y.shape[0]//2, :]
@@ -12,3 +16,23 @@ def fourier_cov(x):
     cov_ys_real = np.cov(ys_real)
 
     return cov_ys_real
+
+
+def main(args=None):
+    pass
+
+
+if __name__ == "__main__":
+    x, y = np.mgrid[0:64:1, 0:64:1]
+
+    X = np.random.rand(64, 64, 100)
+    s = mlab.surf(x, y, X[:, :, 0])
+
+    @mlab.animate
+    def anim():
+        for i in range(100):
+            s.mlab_source.scalars = X[:, :, i]
+            yield
+
+    anim()
+    mlab.show()
