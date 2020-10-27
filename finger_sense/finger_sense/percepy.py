21bcc0491d80cd5559a2d35d73478384e9aaa42f
wngfra <wngfra@gmail.com>
2020-10-27 02:06:34 +0100

    [TODO] realtime signal stack transform to latent space

--- finger_sense/finger_sense/percepy.py
+++ finger_sense/finger_sense/percepy.py
@@ -2,13 +2,18 @@
 # Use of this source code is governed by the Apache-2.0 license, see LICENSE
 
 import numpy as np
-from numpy.fft import fft
+import tensorly as tl
 
 from mayavi import mlab
+from numpy.fft import fft
+from skfda import FDataGrid
+from skfda.representation import basis
 from tensorly.decomposition import tucker
+from tensorly.tenalg import mode_dot
 
 
 def fourier_cov(x):
+    # TODO: Normalize the sample first
     y = fft(x, axis=0) / x.shape[0]
     ys = y[:y.shape[0]//2, :]
     ys_real = np.concatenate([np.real(ys), np.imag(ys)], axis=1)
@@ -23,16 +28,18 @@ def main(args=None):
 
 
 if __name__ == "__main__":
-    x, y = np.mgrid[0:64:1, 0:64:1]
+    x = np.ones((33, 16, 100))
 
-    X = np.random.rand(64, 64, 100)
-    s = mlab.surf(x, y, X[:, :, 0])
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-    @mlab.animate
-    def anim():
-        for i in range(100):
-            s.mlab_source.scalars = X[:, :, i]
-            yield
-
-    anim()
-    mlab.show()
+    core = np.load('core.npy', allow_pickle=True)
+    factors = np.load('factors.npy', allow_pickle=True)
+    projected = mode_dot(x, factors[0].transpose(), 0)
+    projected = mode_dot(projected, factors[1].transpose(), 1)
+    v = np.squeeze(projected)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(v[0,:], v[1,:], v[2,:])
+    plt.show()
