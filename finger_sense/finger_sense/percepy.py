d81df8e41d1b96021f80dedc50508c62a7a3d7e3
wngfra <wngfra@gmail.com>
2020-10-30 16:37:26 +0100

    updated Dockerfiles

--- finger_sense/finger_sense/percepy.py
+++ finger_sense/finger_sense/percepy.py
@@ -14,11 +14,12 @@ def basis_expand(data_matrix, basis):
     fd = FDataGrid(data_matrix.transpose()).to_basis(basis)
     coeffs = fd.coefficients
     coeff_cov = np.cov(coeffs.transpose())
-    
+
     return coeff_cov
+
 
 def project2vec(A, factors):
     projA1 = mode_dot(A, factors[0].transpose(), 0)
     projA2 = mode_dot(projA1, factors[1].transpose(), 1)
-    
+
     return projA2
