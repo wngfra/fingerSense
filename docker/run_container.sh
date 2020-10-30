a5170f5a8196df69bfeac54f286d219d0ab0ba39
wngfra <wngfra@gmail.com>
2020-10-30 15:18:23 +0100

    added mayavi container

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run --gpus all -it --tty --user ubuntu --name $1 -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix wngfra/ros2cuda:libfranka
+docker run -it --tty --device /dev/dri --device /dev/vga_arbiter --gpus all --user ubuntu --name $1 --group-add video -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix wngfra/ros2cuda:libfranka
