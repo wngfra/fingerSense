d36414f85ff2c22c5eb5a1693d53747d08ef2cfd
wngfra <wngfra@gmail.com>
2020-10-30 16:47:16 +0100

    merged multiple Dockerfile

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-docker run --user ubuntu --name franka_dev -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host wngfra/ros2franka:latest
-docker run --device /dev/dri --gpus all --user ubuntu --name visualization_dev -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix wngfra/ros2cuda:visualization
+docker run --device /dev/dri --gpus all --user ubuntu --name visualization_dev -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix wngfra/ros2cuda:franka-dev
