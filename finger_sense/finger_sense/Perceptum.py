b68833f0fda80dae7ea8507c27a87e5d289467ec
wngfra <wngfra@gmail.com>
2021-02-08 23:45:28 +0000

    removed training mode in Perceptum

--- finger_sense/finger_sense/Perceptum.py
+++ finger_sense/finger_sense/Perceptum.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import jax
 import numpy as np
 import pandas as pd
@@ -12,13 +14,13 @@ from finger_sense.utility import KL_divergence_normal, normalize
 
 class Perceptum:
 
-    def __init__(self, dirs, latent_dim, n_basis, stack_size, model_name='Gaussian'):
-        self.model_name = model_name  # default gaussian model
-        self.stack_size = stack_size
+    def __init__(self, dirs, latent_dim, n_basis, model_name='Gaussian'):
         self.latent_dim = latent_dim
-        self.train_stack = []
-        
+        self.model_name = model_name
+
         self.basis = Fourier([0, 2 * np.pi], n_basis=n_basis, period=1)
+        self.jacobian = jax.jacfwd(KL_divergence_normal, 0)
+        self.sensory_memory = deque(maxlen=2*n_basis-1)
 
         self.init_model(dirs)
 
@@ -37,12 +39,11 @@ class Perceptum:
             self.factors = None
             self.info = None
             self.features = None
-            self.count = 0
         else:
             self.core = np.load(dirs['core_dir'], allow_pickle=True).squeeze()
             self.factors = np.load(dirs['factor_dir'], allow_pickle=True)[0:2]
             info = pd.read_csv(dirs['info_dir'], delimiter=',')
-            
+
             self.features = {}
             class_names = info['class_name']
 
@@ -55,10 +56,6 @@ class Perceptum:
                 mean = np.mean(data, axis=1, keepdims=True)
                 cov = np.cov(data)
                 self.features[cn] = [mean, cov]
-
-            self.count = self.core.shape[1]
-
-        self.jacobian = jax.jacfwd(KL_divergence_normal, 0)
 
     def basis_expand(self, data_matrix):
         '''
@@ -111,7 +108,7 @@ class Perceptum:
         else:
             return None
 
-    def perceive(self, M, mode=None):
+    def perceive(self, M):
         '''
             Perceive and process stimulus
 
@@ -121,8 +118,6 @@ class Perceptum:
             ----------
             M : numpy.array
                 Input stimulus matrix in shape (self.stack_size, channel_size)
-            mode : string
-                Perception mode switch (None or 'train')
 
             Returns
             -------
@@ -133,48 +128,30 @@ class Perceptum:
             delta_latent : numpy.array
                 Difference between current and last latent vectors
         '''
-        coeff_cov = self.basis_expand(M) # TODO can raise ValueError
+        coeff_cov = self.basis_expand(M)
+        # latent = self.compress(coeff_cov)
+        latent = np.random.rand(3)
 
-        if mode == 'test':  # With loaded prior knowledge base
-            if self.core is None:
-                self.train_stack = np.array(self.train_stack).transpose(1, 2, 0)
-                core, self.factors = tucker(
-                    self.train_stack, ranks=(3, 1, self.count))
-                self.core = core.squeeze()
-                # TODO: add percept classes
-
-            latent = self.compress(coeff_cov)
-            
-            # Append new latent vector to the core
-            self.core = np.hstack((self.core, latent))
-            self.count += 1
-
-            print(self.core, latent)
-            
+        if len(self.sensory_memory) >= self.sensory_memory.maxlen:
             gradients = np.zeros((len(self.features), self.latent_dim))
             weights = np.zeros((len(self.features), 1))
-            delta_latent = self.core[:, -1] - self.core[:, -2]
+            delta_latent = latent - self.sensory_memory[-1]
 
-            if self.count > self.stack_size:  # Start perception only when a new stack is filled
-                # Slice of last self.stack_size elements
-                stack = self.core[:, self.count - self.stack_size:]
-                p = np.mean(stack, axis=1, keepdims=True), np.cov(stack)
-                y0 = self.core[:, self.count - self.stack_size].reshape(-1, 1)
+            # Get stats of the sensory memory
+            stack = np.array(self.sensory_memory)
+            p = np.mean(stack, axis=0, keepdims=True), np.cov(stack)
+            y0 = self.sensory_memory.pop()
+            self.sensory_memory.append(latent)
 
-                divergences = np.zeros(len(self.features))
+            divergences = np.zeros(len(self.features))
+            # Compute gradients and weights for each percept_class
+            for i, key in enumerate(self.features.keys()):
+                q = self.features[key]
+                divergences[i] = KL_divergence_normal(
+                    latent, p, q, y0, self.stack_size)
+                gradients[i, :] = self.jacobian(
+                    latent, p, q, y0, self.stack_size).reshape(1, -1)
+            exp = np.exp(-divergences)
+            weights = np.reshape(exp / np.sum(exp), (-1, 1))
 
-                # Compute gradients and weights for each percept_class
-                for i, key in enumerate(self.features.keys()):
-                    q = self.features[key]
-                    divergences[i] = KL_divergence_normal(
-                        latent, p, q, y0, self.stack_size)
-                    gradients[i, :] = self.jacobian(
-                        latent, p, q, y0, self.stack_size).reshape(1, -1)
-                exp = np.exp(-divergences)
-                weights = np.reshape(exp / np.sum(exp), (-1, 1))
-
-            return gradients, weights, delta_latent
-
-        elif mode == 'train':  # Without prior, training mode
-            self.train_stack.append(coeff_cov)
-            self.count += 1
+        return gradients, weights, delta_latent
